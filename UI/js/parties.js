59e2dfe797d3c1aa139613d80dc465e1f1493751
kipruto <barxwells@gmail.com>
2019-03-03 19:31:05 +0300

    feat(delete-party) Added delete-party feature for admins [Finishes #164325407]

--- UI/js/parties.js
+++ UI/js/parties.js
@@ -28,14 +28,14 @@ window.onload = function getParties() {
                     '<td>' + party.name + '</td>' +
                     '<td>' + party.hqaddress + '</td>' +
                     '<td><img src=' + party.logourl + '></td>' +
-                    '<td><button id="' + party.id + '" onclick="getParty(this.id)">Edit</button><a href="success.html"><button>Delete</button></a></td>' +
+                    '<td><button id="' + party.id + '" onclick="getParty(this.id)">Edit</button><button id="' + party.id + '" onclick="deleteParty(this.id)">Delete</button></td>' +
                     '</tr>';
             })
         })
 }
 
 function getParty(id) {
-    fetch("https://barno-politico-api.herokuapp.com/api/v2/parties/" + id, {
+    fetch('https://barno-politico-api.herokuapp.com/api/v2/parties/' + id, {
         method: 'GET',
         headers: new Headers({
             'Accept': 'application/json',
@@ -44,8 +44,8 @@ function getParty(id) {
             'Authorization': 'Bearer ' + localStorage.token
         })
     })
-        .then((res) => {
-            return res.json()
+        .then((result) => {
+            return result.json()
         })
         .then((party) => {
             localStorage.name = party['data']['name']
@@ -54,4 +54,30 @@ function getParty(id) {
             localStorage.id = party['data']['id']
             window.location = "edit-party.html"
         })
+}
+
+function deleteParty(id) {
+    answer = confirm('Are you sure you want to delete party?')
+    if (answer) {
+        console.log('confirmed')
+        fetch('https://barno-politico-api.herokuapp.com/api/v2/parties/' + id, {
+            method: 'DELETE',
+            headers: new Headers({
+                'Accept': 'application/json',
+                'Content-type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Authorization': 'Bearer ' + localStorage.token
+            })
+        })
+            .then((result) => {
+                return result.json()
+            })
+            .then(function (party) {
+                if (party['status'] == 200) {
+                    alert(party['message'])
+                    window.location.reload();
+                }
+            })
+
+    }
 }
